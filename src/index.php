def08041066832fda5a0a2113f247bd14edd832a
Ibrahim BinAlshikh <usernane@users.noreply.github.com>
2020-12-03 21:52:53 +0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,4 +1,9 @@
 <?php
+//Show PHP error messages.
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+
 require_once '../vendor/autoload.php';
 require_once 'User.php';
 
